8002c4a80af742f31909f7ef11deba9f5e977a97
Ravi Sinha <ravisinha@my.unt.edu>
2026-01-29 18:51:05 -0600

    POC Herbie usage

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+from herbie import Herbie
+
+
 def main():
     print("Hello from weathersimulatr-backend!")
+
+    # Create Herbie object for the HRRR model 6-hr surface forecast product
+    H = Herbie(
+            '2021-01-01 12:00',
+            model='hrrr',
+            product='sfc',
+            fxx=6
+            )
+
+    # Look at the GRIB2 file contents
+    H.inventory()
+
+    # Download the full GRIB2 file
+    # H.download()
+
+    # Download a subset of the GRIB2 file, like all fields at 500 mb
+    H.download(":500 mb")
+
+    # Read a subset of the file with xarray, like 2-m temperature.
+    H.xarray("TMP:2 m")
 
 
 if __name__ == "__main__":
