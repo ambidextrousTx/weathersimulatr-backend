46a34c9dc2fa8e7d53ea4934a36d0b5ca4e43366
Ravi Sinha <ravisinha@my.unt.edu>
2026-01-31 14:30:14 -0600

    Pull HRRR data for a given timestamp

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from herbie import Herbie
+import argparse
 
 
-def main():
-    print("Hello from weathersimulatr-backend!")
-
-    # Create Herbie object for the HRRR model 6-hr surface forecast product
-    H = Herbie(
-            '2021-01-01 12:00',
-            model='hrrr',
-            product='sfc',
-            fxx=6
-            )
+def load_wind_field(run_time):
+    '''
+    Returns an xrray dataset with
+    u10, v10, wind_speed, wind_dir, lat, lon
+    '''
+    H = Herbie(run_time, model='hrrr', product='sfc', fxx=0)
 
     # Look at the GRIB2 file contents
     H.inventory()
 
-    # Download the full GRIB2 file
-    # H.download()
-
-    # Download a subset of the GRIB2 file, like all fields at 500 mb
-    H.download(":500 mb")
-
     # Read a subset of the file with xarray, like 2-m temperature.
-    H.xarray("TMP:2 m")
+    dataset = H.xarray("GRD:10 m above").herbie.with_wind()[web:15][web:20]
+    print(dataset)
+
+
+def main():
+    print("Hello from weathersimulatr-backend!")
+    parser = argparse.ArgumentParser(
+        description='Process weather data for visualization')
+    parser.add_argument('timestamp', type=str, help='the time stamp for which to get data')
+    args = parser.parse_args()
+
+    load_wind_field(args.timestamp)
 
 
 if __name__ == "__main__":
